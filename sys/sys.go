09a739483aee574400b995fbc4fce6474e466a0f
1343742392 <1343742392@qq.com>
2021-04-01 17:48:35 +0800

    add sys

--- sys/sys.go
+++ sys/sys.go
@@ -5,16 +5,16 @@ import(
 	"runtime"
 )
 
-func Command(command string)(string, error){
+func Command(windcommand string, liunxcommand string)(string, error){
 	if runtime.GOOS == "windows" {
-		cmd := exec.Command("/bin/sh", "-c", command)
+		cmd := exec.Command("cmd", "-c", windcommand)
 		data, err := cmd.Output()
 		if err != nil {
 			return "",err;
 		}
 		return string(data), nil;
 	}else{
-		cmd := exec.Command("/bin/sh", "-c", command)
+		cmd := exec.Command("/bin/sh", "-c", liunxcommand)
 		data, err := cmd.Output()
 		if err != nil {
 			return "",err;
